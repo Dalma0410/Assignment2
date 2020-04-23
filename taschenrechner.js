ed5350d17ec6e59d4ff150f5ba2f8d79596f9c19
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-04-23 17:17:23 +0200

    einabe im Taschenrechner

--- taschenrechner.js
+++ taschenrechner.js
@@ -1,1 +1,24 @@
 taschenrechner.ts
+
+var zahl1;
+var operator;
+var zahl2;
+
+function eingabe(x) {
+    var eingabeFeld = document.getElementById("Display");
+    var lastChar = eingabeFeld.nodeValue.slice(-1);
+
+    if (!Number.isInteger(x) && operator) {
+        alert ("Not possible");
+        return;
+    }
+
+    if (x == '+' || x == '-' || x == '*' || x == '/') {
+        if (lastChar)
+        operator = x;
+    }
+
+    document.getElementById("Display").nodeValue += x;
+}
+
+
